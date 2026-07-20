01eb981d984b6ee260f01f05c9915a6797290ccb
bluiceoficial <murilogomes5792@gmail.com>
2026-07-20 10:51:46 -0300

    Add index publico

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+// Copyright (C) 2026 Murilo Gomes Julio
+// SPDX-License-Identifier: MIT
+
+// Site: https://www.bluice.com.br
+
+if (ini_get('zlib.output_compression') == 'Off') {
+    if (extension_loaded('zlib')) {
+        ob_start('ob_gzhandler');
+    }
+}
+
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+date_default_timezone_set("America/Sao_Paulo");
+
+define('blumiga', true);
+
+require_once(dirname(__FILE__, 2) . '/config/config.php');
+
+if (empty($sessionName)) {
+    session_name('5s0a6d54as8d074as89708qwe98wqdwq');
+} else {
+    session_name($sessionName);
+}
+session_start();
+
+require_once(dirname(__FILE__, 2) . '/core/app.php');
